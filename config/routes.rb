d0a215c62e41cd1f22ef9efa8f9a8d9a8ad1ab1c
João Marcus de Lemos Fernandes <jmldesignergrafico@gmail.com>
2016-04-15 13:43:40 -0300

    Change routes links

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'main_pages#home'
 
-  get 'main_pages/home'
-  get 'main_pages/about'
+  get '/home', to: 'main_pages#home', as: 'home'
+  get '/about', to: 'main_pages#about', as: 'about'
 end
