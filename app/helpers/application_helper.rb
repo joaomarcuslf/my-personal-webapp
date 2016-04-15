d5eee322abed702aaee94fb2d36b6b391e99e24a
João Marcus de Lemos Fernandes <jmldesignergrafico@gmail.com>
2016-04-14 22:56:34 -0300

    Add some Layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,12 @@ module ApplicationHelper
         base_title + " | " + page_title
       end
   end
+  def status(page_title, link)
+    default = "tab-pane"
+    if page_title == link
+      default = default + " active"
+    end
+
+    return default
+  end
 end
