35e100f524fef3a816ead695db3f6ce3049693a3
João Marcus de Lemos Fernandes <jmldesignergrafico@gmail.com>
2016-04-14 21:11:58 -0300

    Finish Main pages

--- spec/features/main_pages/home_spec.rb
+++ spec/features/main_pages/home_spec.rb
@@ -3,6 +3,9 @@ require "rails_helper"
 describe "Viewing home page" do
   it "should be sucessful" do
     visit "/main_pages/home"
+
+    expect(page.title).to include("OuterWorldCodes | Home")
+
     within("section.jumbotron h1.text-center") do
       expect(page).to have_content("We are from Other Space We are from Other Galaxy We are from Other World")
     end
